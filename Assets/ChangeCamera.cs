817a7205376c9ea779ab5bc74a516b7c130c2b0a
ktoyod <ytdh7834@gmail.com>
2018-05-28 01:35:40 +0900

    カメラの視点切り替えができるようになった

---
+++ Assets/ChangeCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCamera : MonoBehaviour
+{
+
+    //　メインカメラ
+    [SerializeField] private GameObject theFirstCamera;
+    //　切り替える他のカメラ
+    [SerializeField] private GameObject theSecondCamera;
+
+    void Start()
+    {
+        theFirstCamera.SetActive(true);
+        theSecondCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // クリックしたらカメラを切り替える
+        if (Input.GetButtonDown("Fire1"))
+        {
+            theFirstCamera.SetActive(!theFirstCamera.activeSelf);
+            theSecondCamera.SetActive(!theSecondCamera.activeSelf);
+        }
+    }
+}
