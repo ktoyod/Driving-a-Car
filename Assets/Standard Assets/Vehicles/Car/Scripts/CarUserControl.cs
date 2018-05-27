ba3947d16ae5b8120267fa8cc45739bf80f315fc
ktoyod <ytdh7834@gmail.com>
2018-05-28 00:01:17 +0900

    車が自動で前進するようになった

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -20,13 +20,14 @@ namespace UnityStandardAssets.Vehicles.Car
         private void FixedUpdate()
         {
             // pass the input to the car!
+            // hの値を顔の傾きによって変化させられるようにしたい
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            // float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
+            m_Car.Move(h, 1, 0, handbrake);
 #else
-            m_Car.Move(h, v, v, 0f);
+            m_Car.Move(h, 1, 0, 0f);
 #endif
         }
     }
