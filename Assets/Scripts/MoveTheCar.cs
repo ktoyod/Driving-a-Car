55240fdda27bf530731deef68837f7f0a5a4cc45
ktoyod <ytdh7834@gmail.com>
2018-05-27 23:47:02 +0900

    車を動かせるようになった

---
+++ Assets/Scripts/MoveTheCar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheCar : MonoBehaviour
+{
+    public float movePower = 1.0f;
+
+    private Rigidbody rb;
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector3(0, 0, movePower));
+    }
+}
