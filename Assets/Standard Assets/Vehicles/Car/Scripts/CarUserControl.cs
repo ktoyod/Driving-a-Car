91505b11009de33055e86234edc3ebd1c87cad60
ktoyod <ytdh7834@gmail.com>
2018-05-28 12:56:20 +0900

    顔の傾きでステアリングできるようにした

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -16,19 +16,29 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Car = GetComponent<CarController>();
         }
 
+        private void Update()
+        {
+            float h = Input.acceleration.x;
+            Debug.Log(h);
+            m_Car.Move(h, 1, 0, 0);
+        }
 
+        /*
         private void FixedUpdate()
         {
             // pass the input to the car!
             // hの値を顔の傾きによって変化させられるようにしたい
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // float h = CrossPlatformInputManager.GetAxis("Horizontal");
             // float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
             m_Car.Move(h, 1, 0, handbrake);
 #else
+            float h = Input.acceleration.x;
+            Debug.Log(h);
             m_Car.Move(h, 1, 0, 0f);
 #endif
         }
+        */
     }
 }
